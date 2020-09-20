756f022008226906a3ee04043e3d14727af87ae0
SamIsland <56886328+SamIsland@users.noreply.github.com>
2020-09-20 21:46:51 +0200

    Add files via upload

---
+++ in a pickle/decode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import pickle
+
+flag = ""
+
+with open('data', 'rb') as f:
+    unpack = pickle.load(f)
+
+print(unpack)
+
+for i in range(1, len(unpack)):
+    try:
+        flag += chr(unpack[i])
+    except:
+        flag += unpack[i]
+
+print(flag)
